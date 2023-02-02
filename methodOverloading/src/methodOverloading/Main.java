973ca1ce10a8212766677aa2e463be52b68ba2cf
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2023-02-02 18:42:21 +0300

    overloading yapısı

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,13 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem=new DortIslem();
+		int sonuc= dortIslem.topla(2, 3);
+		int sonuc2=dortIslem.topla(2, 3, 4);
+		System.out.println(sonuc);
+		System.out.println(sonuc2);
+	}
+
+}
