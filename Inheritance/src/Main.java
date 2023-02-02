f0f86c775c7eca8931701f8aaae3d70c2bff951e
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2023-02-02 18:38:25 +0300

    inheritance yapısı

---
+++ Inheritance/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer customer= new Customer();
+		Employee employee=new Employee();
+		
+		
+		EmployeeManager employeeManager =new EmployeeManager();
+		CustomerManager customerManager=new CustomerManager();
+		
+		customerManager.Add();
+		customerManager.List();
+		
+		employeeManager.Add();
+		employeeManager.List();
+		employeeManager.BestEmployee();
+	}
+
+}
