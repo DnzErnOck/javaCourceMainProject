617c1665c0f162be6c98b8944dde7350076674a7
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2023-02-02 18:35:08 +0300

    class yapıları

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class CustomerManager {
+	public void Add() {
+		System.out.println("Müsteri Eklendi");
+	}
+
+	public void Remove() {
+		System.out.println("Müsteri Silindi");
+	}
+
+	public void Update() {
+		System.out.println("Müsteri Güncellendi");
+	}
+}
