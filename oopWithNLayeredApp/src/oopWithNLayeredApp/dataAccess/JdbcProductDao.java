e3b251d4feca7e30f52294f37e48f1ca135df551
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2023-02-07 17:36:35 +0300

    projeyi katmanlarına ayırdık.

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -0,0 +1,11 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class JdbcProductDao implements ProductDao {
+	public void add(Product product) {
+		//sadece ve sadece db erişim kodları buraya yazılır...sql
+		System.out.println("JDBC ile veritabanına eklendi");
+	}
+	
+}
